389a993b62ec731f96bf7f757e973a0fa823a78c
Flavio Sena <contato@flaviosena.com.br>
2019-01-08 15:16:31 -0200

    fix report week issue

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -5,11 +5,10 @@ class Report extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->model('scape_model');
-		$today = strtotime('today 00:00:00');
-
-		$this_week_start = strtotime('0 week monday 00:00:00');
-		$last_week_start = strtotime('-1 week monday 00:00:00');
+	    $this->load->model('scape_model');
+	    
+	    $this_week_start = $this->find_monday()->format('Y-m-d 00:00:00');
+	    $last_week_start = $this->find_monday(null, 1)->format('Y-m-d 23:59:59');
 
 		$data = $this->scape_model->get(array(
 			'date >=' => date('Y-m-d 00:00:00', strtotime("-30 days"))
@@ -17,7 +16,6 @@ class Report extends CI_Controller {
 
 		$this->data['days30'] = count($data);
 		$this->data['month'] = 0;
-		$this->data['week'] = 0;
 		$this->data['days7'] = 0;
 		$this->data['thisweek'] = 0;
 		$this->data['lastweek'] = 0;
@@ -26,23 +24,42 @@ class Report extends CI_Controller {
 
 		foreach($data as $row) {
 			if(date('Y-m') == date('Y-m', strtotime($row->date))) {
-				$this->data['month']++;
+			    $this->data['month']++;
 			}
 			if(date('Y-m-d 00:00:00', strtotime("-7 days")) <= $row->date) {
 				$this->data['days7']++;
 			}
-			if(date('Y-m-d 00:00:00', $this_week_start) <= $row->date) {
-				$this->data['thisweek']++;
-			}elseif(date('Y-m-d 00:00:00', $last_week_start) <= $row->date) {
-				$this->data['lastweek']++;
+			if($this_week_start <= $row->date) {
+			    $this->data['thisweek']++;
+			}elseif($last_week_start <= $row->date) {
+			    $this->data['lastweek']++;
 			}
 			if(date('Y-m-d', strtotime("-1 days")) == date('Y-m-d', strtotime($row->date))) {
-				$this->data['yesterday']++;
+			    $this->data['yesterday']++;
 			}
 			if(date('Y-m-d') == date('Y-m-d', strtotime($row->date))) {
-				$this->data['today']++;
+			    $this->data['today']++;
 			}
 		}
 		$this->load->view('report', $this->data);
+	}
+    
+	/**
+	 * 
+	 * @param $date
+	 * @param number $number
+	 * @return DateTime
+	 */
+	private function find_monday($date = NULL, $number = 0)
+	{
+	    $date = new DateTime($date);
+
+		while($date->format('w') != 1)
+		{
+			$date->modify('-1 day');
+		}
+		$date->modify('-' . $number . ' weeks');
+		
+		return $date;
 	}
 }
