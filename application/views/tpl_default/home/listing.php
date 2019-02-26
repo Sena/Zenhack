3f97c0e551b1218fddc6b165c44300af5831fe6f
Flavio Sena <contato@flaviosena.com.br>
2019-02-26 17:20:01 -0300

    add bootstrap

--- application/views/tpl_default/home/listing.php
+++ application/views/tpl_default/home/listing.php
@@ -1,22 +1,26 @@
 
 Total: <?=count($post_unread);?>
-<table class="post-list">
-    <tr>
-        <td><strong>Título do post</strong></td>
-        <td><strong>Votos</strong></td>
-        <td><strong>Comentários</strong></td>
-        <td align="right"><strong>Atualização</strong></td>
-    </tr>
-    <?php foreach ($post_unread as $row):?>
-        <tr title="<?=strip_tags($row->details);?>" alt="<?=strip_tags($row->details);?>">
-            <td>
-                <a href="<?=$row->html_url;?>" target="_blank">
-                    <?=$row->title;?>
-                </a>
-            </td>
-            <td><?=$row->vote_sum;?></td>
-            <td><?=$row->comment_count;?></td>
-            <td align="right"><?=$row->br_updated_at;?></td>
+<div class="table-responsive">
+    <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+            <th><strong>Título do post</strong></th>
+            <th><strong>Votos</strong></th>
+            <th><strong>Comentários</strong></th>
+            <th><strong>Atualização</strong></th>
         </tr>
-    <?php endforeach;?>
-</table>
+        </thead>
+        <?php foreach ($post_unread as $row):?>
+            <tr title="<?=strip_tags($row->details);?>" alt="<?=strip_tags($row->details);?>">
+                <td>
+                    <a href="<?=$row->html_url;?>" target="_blank">
+                        <?=$row->title;?>
+                    </a>
+                </td>
+                <td><?=$row->vote_sum;?></td>
+                <td><?=$row->comment_count;?></td>
+                <td align="right"><?=$row->br_updated_at;?></td>
+            </tr>
+        <?php endforeach;?>
+    </table>
+</div>
